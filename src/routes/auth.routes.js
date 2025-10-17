5478c59d573d04403fc7699dd1207d04503fdb92
Prashant Kumar <prashantkumar6425@gmail.com>
2025-10-17 15:44:13 +0530

    add controllers

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -4,9 +4,13 @@ import { validate } from "../middlewares/validator.middleware.js";
 
 import { userLoginValidator, userRegisterValidator } from "../validators/index.js";
 
+import {verifyJWT} from '../middlewares/auth.middleware.js';
+
 const router = Router();
 
 router.route("/register").post(userRegisterValidator(), validate, registerUser)
 router.route("/login").post(userLoginValidator(), validate, login)
 
+// secure routes
+router.route("/login").post(verifyJWT , logoutUser);
 export default router;
